c2d726ffe9905b3ff755dfcfb2931ad3f974e608
Luca Invernizzi <Invernizzi.l@gmail.com>
2015-03-06 15:09:06 -0800

    Dependencies

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo apt-get install -y puppet-common x11-xserver-utils
+sudo apt-get install -y puppet-common
+# For GUIs, also install
+# x11-xserver-utils
 sudo puppet apply recipes/create_sandboxed_toruser.pp
 sudo service tor restart
